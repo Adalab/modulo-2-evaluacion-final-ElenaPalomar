c7591d7be72a0fe13447ae8a398796b9635652ec
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-04-11 13:31:06 +0200

    add styles

--- src/js/3_render.js
+++ src/js/3_render.js
@@ -21,10 +21,10 @@ function renderDrinksHtml(s) {
     if (drink.strDrinkThumb === '') {
       html += ` <img src='https://via.placeholder.com/210x295/ffffff/666666/?text=img' alt='${drink.strDrink}' />`;
     } else {
-      html += ` <img src='${drink.strDrinkThumb}/preview' alt='${drink.strDrink}' />`;
+      html += ` <img src='${drink.strDrinkThumb}/preview' class="drink__image" alt='${drink.strDrink}' />`;
     }
 
-    html += ` <h3>${drink.strDrink}</h3>`;
+    html += ` <h3 class="drink__title">${drink.strDrink}</h3>`;
     html += `</li>`;
   }
 
--- src/js/4_renderFavorites.js
+++ src/js/4_renderFavorites.js
@@ -9,14 +9,14 @@ function renderFavoritesHtml(favorites) {
     html += `<li class="favorite js_favorite" id=${favorite.idDrink}>`;
 
     if (favorite.strDrinkThumb === '') {
-      html += `  <img src='https://via.placeholder.com/210x295/ffffff/666666/?text=img' alt='${favorite.strDrink}' />`;
+      html += `  <img src='https://via.placeholder.com/210x295/ffffff/666666/?text=img' class="favorite__image" alt='${favorite.strDrink}' />`;
     } else {
-      html += `  <img src='${favorite.strDrinkThumb}/preview' alt='${favorite.strDrink}' />`;
+      html += `  <img src='${favorite.strDrinkThumb}/preview' class="favorite__image" alt='${favorite.strDrink}' />`;
     }
 
-    html += `  <h3>${favorite.strDrink}</h3>`;
+    html += `  <h3 class="favorite__title">${favorite.strDrink}</h3>`;
 
-    html += `  <button class="deleteFavorite js_deleteFavorite" id=${favorite.idDrink}><i class="fa-solid fa-circle-xmark"></i></button>`;
+    html += `  <button class="deleteFavorite js_deleteFavorite deleteFavorite__icon" id=${favorite.idDrink}><i class="fa-solid fa-circle-xmark"></i></button>`;
 
     html += `</li>`;
 
