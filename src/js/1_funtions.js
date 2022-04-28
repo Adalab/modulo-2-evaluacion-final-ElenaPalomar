61985d0b0e09c5221d65c247d3a138e159b092db
ElenaPalomar <elenapalomarllorente@gmail.com>
2022-04-28 21:23:15 +0200

    add clean API data

--- src/js/1_funtions.js
+++ src/js/1_funtions.js
@@ -23,7 +23,7 @@ function getFromServer(searchTextValue) {
     .then(response => response.json())
     .then(data => {
 
-      if(data.drink.length === 0) {
+      if(data.drinks.length === 0) {
 
         const errorSearch = document.createElement('h2');
         errorSearch.classList.add('header__title');
@@ -34,19 +34,28 @@ function getFromServer(searchTextValue) {
 
       } else {
 
-        drinks = data.drinks;
+        drinks = data.drinks.map(item => {
+          return {
+            idDrink: item.idDrink,
+            strDrink: item.strDrink,
+            strDrinkThumb: item.strDrinkThumb,
+            strTags: item.strTags
+          };
+        });
+        // Si declaro 'drinks' como constante o como variable (const o let) no me llega a la función renderDrinksHtml(), me llega como que 'drinks' es un array vacío. En cambio si la declaro así a las bravas, todo va como la seda
         renderDrinksHtml();
 
       }
 
     })
-    .catch(() => {
+    .catch((error) => {
       const errorSearch = document.createElement('h2');
       errorSearch.classList.add('header__title');
       const errorSearchText = document.createTextNode('Ha ocurrido un error, prueba de nuevo');
       errorSearch.appendChild(errorSearchText);
       mainContainer.appendChild(errorSearch);
       // Este mensaje se acumula con el de que no haya ningún valor en el campo de búsquedal y con el de que no haya ningún valor que coincida con ninguno de nuestros cockteles, habría que borrar los anteriores antes de que salga este
+      console.log(error);
     });
 }
 
@@ -77,6 +86,7 @@ function addRemoveDrinkToFavorites(event) {
 // Function to render drinks list into Html
 function renderDrinksHtml() {
   let html = '';
+  console.log(drinks);
 
   for (const drink of drinks) {
 
